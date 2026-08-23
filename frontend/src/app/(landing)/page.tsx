39ab62428c4e9cb44f1f9504b94faa3543de1c1f
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2026-08-24 03:15:59 +0500

    style: remove testimonials section from landing page

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
 import { HowItWorks } from "./components/HowItWorks";
-import { Testimonials } from "./components/Testimonials";
 import { Pricing } from "./components/Pricing";
 import { CTA } from "./components/CTA";
 import { Navigation } from "./components/Navigation";
@@ -14,7 +13,6 @@ export default function LandingPage() {
       <Hero />
       <Features />
       <HowItWorks />
-      <Testimonials />
       <Pricing />
       <CTA />
       <Footer />
